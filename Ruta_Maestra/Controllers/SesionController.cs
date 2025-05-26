ca2b19a524fa1238a6a1f1aa26dd171c204df412
Eliobys Ogaza <Eliobys.Ogaza@TRANESMEDELLIN.LOCAL>
2025-05-26 14:58:41 -0500

    starting proyect

---
+++ Ruta_Maestra/Controllers/SesionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ruta_Maestra.Controllers
+{
+    public class SesionController : Controller
+    {
+        // GET: Sesion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
